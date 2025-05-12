8e57673e93f08ef6ba1af5427311da283d877297
Jaime Lopez <jaimelopezmislata@gmail.com>
2025-05-12 09:23:55 +0200

    New functions between index & spa

--- client/spa.js
+++ client/spa.js
@@ -1,18 +1,31 @@
 const bookNav = document.getElementById("bookNav");
 const memberNav = document.getElementById("memberNav");
+const membersNav = document.getElementById("membersNav");
 const getBooksDiv = document.getElementById("getBooks");
 const newMemberDiv = document.getElementById("newMember");
+const getMembersDiv = document.getElementById("getMembers");
+
+const hideView = () => {
+    getBooksDiv.classList.add("hide");
+    newMemberDiv.classList.add("hide");
+    getMembersDiv.classList.add("hide");
+}
 
 const goBooks = () => {
-    newMemberDiv.classList.add("hide");
+    hideView();
     getBooksDiv.classList.remove("hide");
 }
 
-const goMember = () => {
+const goNewMember = () => {
+    hideView();
     newMemberDiv.classList.remove("hide");
-    getBooksDiv.classList.add("hide");
+}
+
+const goMembers = () => {
+    hideView();
+    getMembersDiv.classList.remove("hide");
 }
 
 bookNav.addEventListener("click", goBooks);
-memberNav.addEventListener("click", goMember);
-
+memberNav.addEventListener("click", goNewMember);
+membersNav.addEventListener("click", goMembers);
