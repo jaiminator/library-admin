0c3fd469a4085b792cf935b3cdbaad1df10fd2e7
Jaime Lopez <jaimelopezmislata@gmail.com>
2025-05-12 21:14:31 +0200

    Implementado registro de socio en cliente

--- client/script.js
+++ client/script.js
@@ -3,6 +3,11 @@ const BASE_URL = "http://localhost:8000";
 
 const booksContainer = document.getElementById("booksContainer");
 const btnGetBooks = document.getElementById("btnGetBooks");
+const btnCreateMember = document.getElementById("btnCreateMember");
+
+const inputName = document.getElementById("inputName");
+const inputUser = document.getElementById("inputUser");
+const inputPass = document.getElementById("inputPass");
 
 const getBooks = () => {
   fetch(BASE_URL + "/books")
@@ -10,17 +15,45 @@ const getBooks = () => {
   .then((data) => {
     booksContainer.innerHTML = "";
     data.forEach(book => {
-      const {id, title, description, author, publication_year} = book;
+      const {id, title, description, author, publication_year, ISBN} = book;
       booksContainer.innerHTML += `<p>
       ${id}
       ${title}
       ${description}
       ${author}
       ${publication_year}
+      ${ISBN}
       </p>`;
     });
     
   })
 }
 
+const newMember = () => {
+  const nameToCreate = inputName.value;
+  const userToCreate = inputUser.value;
+  const passToCreate = inputPass.value;
+
+  fetch(BASE_URL + "/register", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      name: nameToCreate,
+      username: userToCreate,
+      password: passToCreate
+    }),
+  }).then(() => {
+    inputName.value = "";
+    inputUser.value = "";
+    inputPass.value = "";
+    alert("Member registed");
+  })
+  .catch(() => {
+    alert("Error to register member");
+  });
+};
+
 btnGetBooks.addEventListener("click", getBooks);
+btnCreateMember.addEventListener("click", newMember);
